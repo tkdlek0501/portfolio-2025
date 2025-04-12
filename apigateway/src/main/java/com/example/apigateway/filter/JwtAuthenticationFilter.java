49d4ba6b802e6fad55bcfa17b36474e0c1465455
tkdlek0501 <tkdlek0501@naver.com>
2025-04-12 18:54:45 +0900

    feat : gateway module (Checking 401 error)

--- apigateway/src/main/java/com/example/apigateway/filter/JwtAuthenticationFilter.java
+++ apigateway/src/main/java/com/example/apigateway/filter/JwtAuthenticationFilter.java
@@ -8,14 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ServerWebExchange;
 
 import java.io.IOException;
 import java.util.Arrays;
--- user/src/main/java/com/example/user/config/SecurityConfig.java
+++ user/src/main/java/com/example/user/config/SecurityConfig.java
@@ -1,20 +1,31 @@
 package com.example.user.config;
 
-import com.example.user.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailsService userDetailsService;
+//    private final CustomUserDetailsService userDetailsService;
 
     // 비밀번호 인코더 Bean 등록
     @Bean
@@ -28,13 +39,46 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
-    // AuthenticationManagerBuilder 설정 (사용자 정보 조회 + 비밀번호 비교 방식 등록)
+    // 해당 모듈에서는 별도의 인증/인가 처리 안함 -> apigateway 에서 처리
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService); // 사용자 조회 서비스
-        authProvider.setPasswordEncoder(passwordEncoder());     // 비밀번호 검증 방식
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                //FormLogin, BasicHttp, logout 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(
+                        httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT))
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/swagger-ui/**",    // Swagger UI 관련 리소스
+                                "/v3/api-docs/**",   // Swagger API 문서
+                                "/swagger-resources/**",  // Swagger 리소스
+                                "/webjars/**",      // Swagger WebJars 리소스
+                                "/swagger-ui.html"  // Swagger UI HTML 페이지
+                        ).permitAll()  // Swagger 관련 리소스 인증 없이 접근 허용
+                        .anyRequest().permitAll()  // 나머지 경로는 인증 없이 접근 허용
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .build();
+    }
 
-        return authProvider;
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOriginPatterns(List.of("*"));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            config.setAllowedHeaders(List.of("*"));
+            config.setAllowCredentials(true);
+            config.setMaxAge(3600L);
+            return config;
+        };
     }
 }
